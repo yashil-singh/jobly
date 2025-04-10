99eecc05da1f65e364eabdcd438f85ce43c803b1
Yashil Lal Singh <yashilsingh0208@gmail.com>
2025-04-10 13:02:55 +0545

    Updated base api url

--- client/src/lib/constants.ts
+++ client/src/lib/constants.ts
@@ -1,4 +1,4 @@
-export const BASE_API_URL = "http://localhost:3000/api";
+export const BASE_API_URL = "https://jobly-djl4.onrender.com/api";
 export const PASSWORD_REGEX =
   /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]).+$/;
 export const URL_REGEX =
